e7aff09d77bf34ea463110d012242e819de90ed6
Evan Boettner <eboettner@icstars.org>
2023-09-26 00:20:21 -0500

    mapped buttons and added very basic functionality

--- index.js
+++ index.js
@@ -1,6 +1,71 @@
-let calcWindow = document.getElementById('window');
+let calcWindow = document.getElementById("window");
+let numbers = document.querySelector(".numbers");
+let firstGroup = document.getElementById("operators1");
+let secondGroup = document.getElementById("operators2");
+let numberArray = [7, 8, 9, 4, 5, 6, 1, 2, 3];
+let firstArray = ["(", "X", "+"];
+let secondArray = [")", "/", "-"];
+let equal = document.querySelector(".equal");
+var firstNumber;
+var secondNumber;
+var operator;
 
-calcWindow.addEventListener(('click'), () => {
-    console.log('hello')
-    calcWindow.innerHTML = "<p>Hello World</p>"
-}) 
+firstGroup.innerHTML = firstArray
+  .map(
+    (operator, index) =>
+      `<div class="operator" id="${index}op1">${operator}</div>`
+  )
+  .join("");
+
+secondGroup.innerHTML = secondArray
+  .map(
+    (operator, index) =>
+      `<div class="operator" id="${index}op2">${operator}</div>`
+  )
+  .join("");
+
+numbers.innerHTML = numberArray
+  .map((number, index) => `<div class="number" id=${index}>${number}</div>`)
+  .join("");
+
+for (let i = 0; i < numberArray.length; i++) {
+  document.getElementById(i).addEventListener("click", () => {
+    calcWindow.innerHTML += `<span>${
+      document.getElementById(i).textContent
+    }</span>`;
+  });
+}
+
+for (let i = 0; i < firstArray.length; i++) {
+  document.getElementById(`${i}op1`).addEventListener("click", () => {
+    firstNumber = Number(calcWindow.textContent);
+    calcWindow.innerHTML += `<span>${
+      document.getElementById(`${i}op1`).textContent
+    }</span>`;
+    operator = document.getElementById(`${i}op1`).textContent;
+  });
+  document.getElementById(`${i}op2`).addEventListener("click", () => {
+    firstNumber = Number(calcWindow.textContent);
+    calcWindow.innerHTML += `<span>${
+      document.getElementById(`${i}op2`).textContent
+    }</span>`;
+    operator = document.getElementById(`${i}op2`).textContent;
+  });
+}
+
+equal.addEventListener("click", () => {
+  let math = calcWindow.textContent;
+  secondNumber = math.substring(Number(math.indexOf(operator)) + 1);
+  if (operator === "/") {
+    calcWindow.textContent = Number(firstNumber) / Number(secondNumber);
+  }
+  if (operator === "X") {
+    calcWindow.textContent = Number(firstNumber) * Number(secondNumber);
+  }
+  if (operator === "+") {
+    calcWindow.textContent = Number(firstNumber) + Number(secondNumber);
+  }
+  if (operator === "-") {
+    calcWindow.textContent = Number(firstNumber) - Number(secondNumber);
+  }
+});
